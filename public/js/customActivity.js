2b6d18cc23f23d479ccdb1a132d6cf441d5aedd2
shweta48-creator <63191476+shweta48-creator@users.noreply.github.com>
2022-02-11 15:38:54 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -101,7 +101,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.Custom_SMS.twilioNumber}}" ,//<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.twiliomsgsend.phonenumber}}" ,//<----This should map to your data extension name and phone number column
            
         }];       
         payload['metaData'].isConfigured = true;
