fcc6ffa68eeecf24c046044d8a8254c92cddeb62
shweta48-creator <63191476+shweta48-creator@users.noreply.github.com>
2022-02-28 12:20:51 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -161,21 +161,21 @@ exports.execute = function(req, res) {
                    // yaha se start hora h 
                     const TrackingData = {
                         "items": [{
-                            //"Email": uniqueEmail,
-                           // "Status": message.status,
-                            //"AccountSID": message.accountSid,
-                            //"apiVersion": message.apiVersion,
-                            //"Body": message.body,
+                            "Email": uniqueEmail,
+                            "Status": message.status,
+                            "AccountSID": message.accountSid,
+                            "apiVersion": message.apiVersion,
+                            "Body": message.body,
                             "dateCreated": message.dateCreated,
                             "dateUpdated": message.dateUpdated,
-                            //"dateSent": message.dateSent,
-                            //"direction": message.direction,
-                            //"from": message.from,
-                           // "messagingServiceSid": message.messagingServiceSid,
-                            //"price": message.price,
-                            //"priceUnit": message.priceUnit,
-                            //"sid": message.sid,
-                            //"uri": message.uri
+                            "dateSent": message.dateSent,
+                            "direction": message.direction,
+                            "from": message.from,
+                            "messagingServiceSid": message.messagingServiceSid,
+                            "price": message.price,
+                            "priceUnit": message.priceUnit,
+                            "sid": message.sid,
+                            "uri": message.uri
                         }]
                     }
                     console.log(TrackingData);
