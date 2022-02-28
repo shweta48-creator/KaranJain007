610a6875625a2287c716715a6501bdeb3e21255a
shweta48-creator <63191476+shweta48-creator@users.noreply.github.com>
2022-02-28 15:05:24 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -181,7 +181,7 @@ exports.execute = function(req, res) {
                     //data extension me insert krwana hai ..
                     request.put({
                         headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + accessToken },
-                        url: restURL + '/data/v1/async/dataextensions/key:CE448648-91CF-4E70-AC45-38C61CE83C2C/rows',
+                        url: restURL + '/data/v1/async/dataextensions/key:664D8BDB-223A-46CA-BADF-E8BD4E7DD45C/rows',
                         body: TrackingData,
                         json: true
                     }, function(error, response, body) {
